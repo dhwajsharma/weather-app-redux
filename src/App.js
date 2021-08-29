83719b9fea45526b2b58e31e5d3d37ed5ebe16c3
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-08-29 10:15:57 +0530

    Implementing Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
+  const [place, setPlace] = useState("")
+  const [placeData, setPlaceData] = useState({})
+
+  const updatePlaceData = () => {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${place}`)
+      .then(res => res.json())
+      .then(data => {
+        setPlaceData(data);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 form">
+            <input type="text" placeholder="Enter city name..." value={place} onChange={(e) => setPlace(e.target.value)} />
+            <button type="button" className="btn btn-primary" onClick={updatePlaceData}>Submit</button>
+          </div>
+          <div className="offset-md-4 col-12 col-md-4 weather">
+            <div className="card">
+              {placeData.location ? (
+                <div>
+                  <img loading="lazy" src={placeData.current.condition.icon} alt="" />
+                  <div className="temp">{placeData.current.temp_c}°</div>
+                  <div className="desc">{placeData.current.condition.text}</div>
+                  <div className="place">{placeData.location.name}</div>
+                  <div className="container">
+                    <div className="row whp">
+                      <div className="col">
+                        <div className="title">Wind Now</div>
+                        <div className="data">{placeData.current.wind_kph}<span className="unit">KM</span> </div>
+                      </div>
+                      <div className="col">
+                        <div className="title">Humidity</div>
+                        <div className="data">{placeData.current.humidity}<span className="unit">%</span> </div>
+                      </div>
+                      <div className="col">
+                        <div className="title">Precipitation</div>
+                        <div className="data">{placeData.current.precip_in}<span className="unit">%</span> </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <h2>Place not found</h2>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
